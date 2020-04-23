28a83ac0b8d2f544d7e5ee34b2f81a06f340c6a5
Vishal Haria <vishal@designbyvh.com>
2020-04-23 18:45:59 +0700

    Changed to Mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
+  :address => 'smtp.mailgun.org',
   :port => '587',
   :authentication => :plain,
-  :user_name => Rails.application.credentials.sendgrid[:username],
-  :password => Rails.application.credentials.sendgrid[:password],
-  :domain => 'heroku.com',
+  :user_name => 'postmaster@sandboxc5c164aa4d8648ea909de143b36cee12.mailgun.org', #Rails.application.credentials.sendgrid[:username],
+  :password => '716206cd2262b0832e1142d89c8d01e7-f135b0f1-1af2ae83', #Rails.application.credentials.sendgrid[:password],
+  :domain => 'sandboxc5c164aa4d8648ea909de143b36cee12.mailgun.org',
   :enable_starttls_auto => true
 }
