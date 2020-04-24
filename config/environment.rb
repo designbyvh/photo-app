a82926d7073789fac4114c23fdc4d22517d83082
Vishal Haria <vishal@designbyvh.com>
2020-04-24 19:57:02 +0700

    Updated credentials

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.mailgun.org',
   :port => '587',
   :authentication => :plain,
-  :user_name => 'postmaster@sandboxc5c164aa4d8648ea909de143b36cee12.mailgun.org', #Rails.application.credentials.sendgrid[:username],
-  :password => '716206cd2262b0832e1142d89c8d01e7-f135b0f1-1af2ae83', #Rails.application.credentials.sendgrid[:password],
-  :domain => 'sandboxc5c164aa4d8648ea909de143b36cee12.mailgun.org',
+  :user_name => Rails.application.credentials.mailgun[:user_name],
+  :password => Rails.application.credentials.mailgun[:password],
+  :domain => Rails.application.credentials.mailgun[:domain],
   :enable_starttls_auto => true
 }
